b5602288ea724b9fb11284dba0414646f92f91ee
Ejnar Arechavala <ejnar.arechavala@onepeloton.com>
2021-10-11 12:33:52 -0400

    add embed, leaderboards, steps graph

--- bot/cogs/fitbot.py
+++ bot/cogs/fitbot.py
@@ -1,12 +1,13 @@
 import logging
 
-from discord import Colour, Embed
+from discord import Colour, Embed, File
 from discord.ext import commands
 from fitbit.api import Fitbit
 
-from lib.fitbot.client import FitbotService
+from lib.fitbot.service import FitbotService, GuildWeeklyStats
 from lib.fitbot.config import FitbotConfig
 from lib.utils.consts import HexColors
+from lib.utils.graph import generate_line_plot_image, generate_line_plot, write_fig_to_tempfile
 from lib.utils.types import hex_to_rgb
 
 
@@ -68,13 +69,79 @@ class Fitbot(commands.Cog):
         await ctx.reply("You've been disconnected from Fitbot")
 
     @commands.command(description="Get Fitbit stats")
-    async def fitbot_stats(self, ctx):
-        pass
+    async def fitbot_leaderboard(self, ctx):
+        stats = self.fitbot.get_guild_weekly_stats(ctx.message.guild.id)
+        user_id_to_username = {}
+        for user_id in stats.user_ids:
+            user = await self.bot.fetch_user(int(user_id))
+            if user and user.name:
+                user_id_to_username[user_id] = user.name
+        response = WeeklyLeaderboardResponse(stats, user_id_to_username)
+        await ctx.send(embed=response.to_embed(), file=response.image_file)
 
     @fitbot_register.error
     async def fitbot_register_error(self, ctx, error):
         logging.error(f"Fitbot Registration Error: {error}")
         await ctx.send(f"Fitbot Error: {error.original}")
+
+
+class WeeklyLeaderboardResponse:
+
+    def __init__(self, guild_stats: GuildWeeklyStats, user_id_map):
+        self.guild_stats = guild_stats
+        self.user_id_map = user_id_map
+
+    @property
+    def color(self):
+        return Colour.from_rgb(*hex_to_rgb(HexColors.FITBIT_BLUE))
+
+    @property
+    def description(self):
+        if not self.guild_stats.user_ids:
+            return "This server has no registered fitbit accounts"
+
+        am_lb = self.guild_stats.weekly_active_minutes_leaderboard
+        description = "**Weekly Active Minutes**"
+        for idx, lb_entry in enumerate(am_lb, start=1):
+            description += f"\n{idx}. <@!{lb_entry.user_id}>: {lb_entry.score}"
+
+        weekly_steps_lb = self.guild_stats.weekly_steps_leaderboard
+        description += "\n\n**Weekly Steps**"
+        for idx, lb_entry in enumerate(weekly_steps_lb, start=1):
+            description += f"\n{idx}. <@!{lb_entry.user_id}>: {lb_entry.score}"
+
+        last_day_steps_lb = self.guild_stats.last_day_steps_leaderboard
+        description += "\n\n**Last Day Steps**"
+        for idx, lb_entry in enumerate(last_day_steps_lb, start=1):
+            description += f"\n{idx}. <@!{lb_entry.user_id}>: {lb_entry.score}"
+
+        return description
+
+    @property
+    def thumbnail_image_url(self):
+        return "https://icon-library.com/images/fitbit-icon/fitbit-icon-20.jpg"
+
+    @property
+    def image_file(self):
+        df = self.guild_stats.steps_df
+        fig = generate_line_plot(df, x=df.index, y=list(df.columns))
+        fig.for_each_trace(lambda t: t.update(name=self.user_id_map.get(t.name, t.name)))
+        fig.update_layout(
+            xaxis_title="Date",
+            yaxis_title="Steps",
+            legend_title="Users"
+        )
+        return File(write_fig_to_tempfile(fig), filename="image.png")
+
+    def to_embed(self):
+        embed = Embed(
+            title="Fitbit Leaderboard",
+            description=self.description,
+            color=self.color
+        )
+        embed.set_thumbnail(url=self.thumbnail_image_url)
+        embed.set_image(url="attachment://image.png")
+        return embed
 
 
 def setup(bot):
--- bot/lib/fitbot/service.py
+++ bot/lib/fitbot/service.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from functools import cached_property
 
 import attr
+import pandas as pd
 from fitbit import Fitbit
 
 from db.models import ThirdPartyAuth
@@ -38,7 +39,7 @@ class FitbotService:
         logging.info(f"TOKEN: {token}")
         third_party_auth = ThirdPartyAuth.objects.create(
             user_id=user_id,
-            provider=cls.provider,
+            provider=cls.config.provider,
             guild_id=guild_id,
             access_token=token['access_token'],
             refresh_token=token['refresh_token'],
@@ -83,26 +84,22 @@ class FitbotService:
         user_stats = [cls.get_user_weekly_stats(auth) for auth in user_auths]
         return GuildWeeklyStats(user_stats)
 
+    @staticmethod
+    def _get_time_series(client, resource, period='7d'):
+        return client.time_series(
+            f"activities/{resource}",
+            period=period
+        ).get(f"activities-{resource}")
+
     @classmethod
     def get_user_weekly_stats(cls, auth):
         client = cls.get_authed_client(auth=auth)
-        period = '7d'
-        fairly_active = client.time_series("activities/minutesFairlyActive", period=period)
-        very_active = client.time_series("activities/minutesVeryActive", period=period)
-        steps = client.time_series("activities/steps", period=period)
-        distance = client.time_series("activities/distance", period=period)
-
-        logging.info(f"Fairly Active: {fairly_active}")
-        logging.info(f"Very Active: {very_active}")
-        logging.info(f"Steps: {steps}")
-        logging.info(f"Distance: {distance}")
-
         return UserWeeklyStats(
             user_id=auth.user_id,
-            fairly_active=fairly_active,
-            very_active=very_active,
-            steps=steps,
-            distance=distance
+            fairly_active=cls._get_time_series(client, "minutesFairlyActive"),
+            very_active=cls._get_time_series(client, "minutesVeryActive"),
+            steps=cls._get_time_series(client, "steps"),
+            distance=cls._get_time_series(client, "distance")
         )
 
 
@@ -117,40 +114,111 @@ class GuildWeeklyStats:
     def __init__(self, user_stats):
         self.user_stats = user_stats
 
-    @cached_property
-    def leaderboard(self):
+    @property
+    def weekly_active_minutes_leaderboard(self):
+        return self._leaderboard('total_active_minutes')
+
+    @property
+    def last_day_steps_leaderboard(self):
+        return self._leaderboard('last_day_steps')
+
+    @property
+    def weekly_steps_leaderboard(self):
+        return self._leaderboard('total_steps')
+
+    def _leaderboard(self, metric):
         return sorted(
             [
                 LeaderboardEntry(
                     user_id=user_stat.user_id,
-                    score=user_stat.total_active_minutes
+                    score=getattr(user_stat, metric)
                 ) for user_stat in self.user_stats
             ],
             key=lambda lb_entry: lb_entry.score,
             reverse=True
         )
 
+    @property
+    def user_ids(self):
+        return [u.user_id for u in self.user_stats]
+
+    @property
+    def steps_df(self):
+        return self._metric_df('steps')
+
+    @cached_property
+    def distance_df(self):
+        return self._metric_df("distance")
+
+    def _metric_df(self, metric):
+        data = dict()
+        for user_stat in self.user_stats:
+            metric_data = getattr(user_stat, metric)
+            index = user_stat.metric_days[-len(metric_data):]
+            series = pd.Series(metric_data, index=index)
+            data[user_stat.user_id] = series
+        return pd.DataFrame(data)
+
 
 class UserWeeklyStats:
     def __init__(self, user_id, fairly_active, very_active, steps, distance):
         self.user_id = user_id
-        self.fairly_active_min_data = fairly_active
-        self.very_active_min_data = very_active
-        self.steps_data = steps
-        self.distance_data = distance
+        self._fairly_active_min_data = fairly_active
+        self._very_active_min_data = very_active
+        self._steps_data = steps
+        self._distance_data = distance
+
+    @staticmethod
+    def _time_series_values(series, data_type):
+        return [data_type(d['value']) for d in series]
+
+    @property
+    def metric_days(self):
+        return [
+            datetime.strptime(d['dateTime'], "%Y-%m-%d")
+            for d in self._steps_data
+        ]
+
+    @property
+    def steps(self):
+        return self._time_series_values(self._steps_data, int)
+
+    @property
+    def distance(self):
+        return self._time_series_values(self._distance_data, float)
+
+    @property
+    def fairly_active_minutes(self):
+        return self._time_series_values(self._fairly_active_min_data, int)
+
+    @property
+    def very_active_minutes(self):
+        return self._time_series_values(self._very_active_min_data, int)
+
+    @property
+    def last_day_steps(self):
+        return self.steps[-1]
 
     @property
     def total_steps(self):
-        return sum(self.steps_data)
+        return sum(self.steps)
 
     @property
     def total_distance(self):
-        return sum(self.distance_data)
+        return sum(self.distance)
+
+    @property
+    def total_fairly_active_minutes(self):
+        return sum(self.fairly_active_minutes)
+
+    @property
+    def total_very_active_minutes(self):
+        return sum(self.very_active_minutes)
 
     @property
     def total_active_minutes(self):
         # per fitbit docs, active minutes = sum of fairly & very active min
-        return sum(self.fairly_active_min_data) + sum(self.very_active_min_data)
+        return self.total_fairly_active_minutes + self.total_very_active_minutes
 
 
 class FitbitTokenRefresher:
